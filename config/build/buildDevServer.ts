280077bbe40248b2a270af93444de177c7a4e822
Ormina <ecolog321@gmail.com>
2025-06-23 18:19:52 +0800

    lazy load + routing

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -4,6 +4,7 @@ import type { Configuration } from "webpack-dev-server";
 export function buildDevServer(options: BuildOptions): Configuration {
     return {
         port: options.PORT,
-        open:true
+        open:true,
+        historyApiFallback:true,
     }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
+import { Header } from "./components/Header";
+import "./index.scss";
+import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Suspense } from "react";
 
-import React from 'react';
-import { Header } from './components/Header';
-import './index.scss'
-
-export const App=() =>{
+export const App = () => {
   return (
-    <div className='app'>
-    <Header/>
+    <div className="app">
+      <Link to={"/"}>Главная</Link>
+      <Link to={"about"}>О стайте</Link>
+      <Suspense fallback={<div>Загрузка...</div>}>
+      <Routes>
+          <Route path="/about" element={<AboutPageAsync />} />
+          <Route path="/" element={<MainPageAsync />} />
+      </Routes>
+      </Suspense>
+      <Header />
     </div>
-  )
-}
+  );
+};
 
 export default App;
-
