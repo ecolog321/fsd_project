ca30f03b8f27d1975fc6b66f12ea744dda9bfd52
Ormina <ecolog321@gmail.com>
2025-06-24 11:08:56 +0800

    add themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import { Header } from "./components/Header";
-import "./index.scss";
+import "./styles/index.scss";
 import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 import { Suspense } from "react";
+import { useTheme } from "./context/ThemeContext/useTheme";
 
 export const App = () => {
+
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Change Theme</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"about"}>О стайте</Link>
       <Suspense fallback={<div>Загрузка...</div>}>
-      <Routes>
+        <Routes>
           <Route path="/about" element={<AboutPageAsync />} />
           <Route path="/" element={<MainPageAsync />} />
-      </Routes>
+        </Routes>
       </Suspense>
       <Header />
     </div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import { render } from "react-dom";
 import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
+import ThemeProvider from "./context/ThemeContext/ThemeProvider";
 
 render(
   <BrowserRouter>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </BrowserRouter>,
   document.getElementById("root")
 );
