54030aac2e7895d763bfefd508d0036efc8e58c1
Ormina <ecolog321@gmail.com>
2025-06-25 10:56:46 +0800

    router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,9 @@
 
 import "./styles/index.scss";
-import { Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { Suspense } from "react";
 import { classNames } from "shared/lib/helpers/classNames/classNames";
 import { useTheme } from "./provider/ThemeProvider/lib/useTheme";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
+import { AppRouter } from "./provider/router";
 
 export const App = () => {
 
@@ -17,12 +14,7 @@ export const App = () => {
       <button onClick={toggleTheme}>Change Theme</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"about"}>О стайте</Link>
-      <Suspense fallback={<div>Загрузка...</div>}>
-        <Routes>
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/" element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter/>
     </div>
   );
 };
