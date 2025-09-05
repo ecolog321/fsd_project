7d82e3b78330917632a5fdff0b905c70601889d0
Ormina <ecolog321@gmail.com>
2025-09-05 15:08:50 +0800

    add chromatic, screenshot test

--- config/.storybook/preview.ts
+++ config/.storybook/preview.ts
@@ -5,6 +5,37 @@ import {ThemeDecorator} from '../../src/shared/config/storybook/ThemeDecorator/T
 import {RouterDecorator} from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator'
 import {Theme} from "../../src/app/providers/ThemeProvider"
 
+const customViewports = {
+  iphoneSE: {
+    name: 'iPhone SE',
+    styles: {
+      width: '375px',
+      height: '667px',
+    },
+  },
+  iphone12: {
+    name: 'iPhone 12',
+    styles: {
+      width: '390px',
+      height: '844px',
+    },
+  },
+  ipad: {
+    name: 'iPad',
+    styles: {
+      width: '768px',
+      height: '1024px',
+    },
+  },
+  macbook: {
+    name: 'MacBook Pro',
+    styles: {
+      width: '1440px',
+      height: '900px',
+    },
+  },
+};
+
 const preview: Preview = {
   parameters: {
     controls: {
@@ -12,6 +43,17 @@ const preview: Preview = {
        color: /(background|color)$/i,
        date: /Date$/i,
       },
+    },
+     viewport: {
+      viewports: customViewports,
+      defaultViewport: 'desktop',
+    },
+    
+    // Настройки для Chromatic
+    chromatic: {
+      viewports: [320, 375, 768, 1440],
+      pauseAnimationAtEnd: true,
+      delay: 100,
     },
   },
   decorators:[
