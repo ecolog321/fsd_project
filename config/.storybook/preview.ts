111c3586699cbd52d5d16156d9005c1adefcee62
Ormina <ecolog321@gmail.com>
2025-09-11 16:54:22 +0800

    change import Decorator

--- config/.storybook/preview.ts
+++ config/.storybook/preview.ts
@@ -1,7 +1,7 @@
 import type { Preview } from '@storybook/react-webpack5'
 import '../../src/app/styles/index.scss'
 import {StyleDecorator} from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator'
-import {ThemeDecorator} from '../../src/shared/config/storybook/ThemeDecorator/ThemeSecorator'
+import {ThemeDecorator} from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator'
 import {RouterDecorator} from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator'
 import {Theme} from "../../src/app/providers/ThemeProvider"
 
