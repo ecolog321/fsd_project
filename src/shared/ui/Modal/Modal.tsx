6e4f948b27306e7ff9b88fcef0092cda5dae9cad
Ormina <ecolog321@gmail.com>
2025-09-12 17:25:47 +0800

    add theme for body

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -2,7 +2,6 @@ import { classNames } from "shared/lib/classNames/classNames";
 import cls from "./Modal.module.scss";
 import { ReactNode, useCallback, useEffect, useRef, useState } from "react";
 import Portal from "../Portal/Portal";
-import { useTheme } from "app/providers/ThemeProvider";
 
 interface ModalProps {
   className?: string;
@@ -13,7 +12,7 @@ interface ModalProps {
 
 function Modal(props: ModalProps) {
   const { className, children, isOpen, onClose } = props;
-  const {theme}=useTheme();
+
 
   const [isClosing, setIsClosing] = useState(false);
 
@@ -60,7 +59,7 @@ function Modal(props: ModalProps) {
 
   return (
       <Portal>
-          <div className={classNames(cls.modal, mods, [className, theme])}>
+          <div className={classNames(cls.modal, mods, [className])}>
               <div className={cls.overlay} onClick={closeHandler}>
                   <div className={cls.content} onClick={onContentClick}>
                       {children}
