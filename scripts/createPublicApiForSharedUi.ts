6739474c74dba5e94c034b27817a3119cbe210af
Ormina <ecolog321@gmail.com>
2026-05-28 17:57:15 +0800

    finish func createPublicApi

--- scripts/createPublicApiForSharedUi.ts
+++ scripts/createPublicApiForSharedUi.ts
@@ -1,4 +1,4 @@
-import path from 'path'
+import path from "path";
 import { Project } from "ts-morph";
 
 const project = new Project({});
@@ -7,18 +7,25 @@ project.addSourceFilesAtPaths("src/**/*.ts");
 project.addSourceFilesAtPaths("src/**/*.tsx");
 
 const files = project.getSourceFiles();
-const uiPath = path.resolve(__dirname, "..", "..", "src", "shared", "ui");
+const uiPath = path.resolve(__dirname, "..", "src", "shared", "ui");
 const sharedDir = project.getDirectory(uiPath);
 const componentDirs = sharedDir?.getDirectories();
 
-function isAbsolute(value:string) {
-    const layers = ['app', 'shared', 'entities','features', 'widgets','pages'];
-    return layers.some(layer => value.startsWith(layer));
-
-    componentDirs?.forEach((directory) =>{
-        console.log(directory.getBaseName())
-    })
+function isAbsolute(value: string) {
+  const layers = ["app", "shared", "entities", "features", "widgets", "pages"];
+  return layers.some((layer) => value.startsWith(layer));
 }
+
+componentDirs?.forEach((directory) => {
+  const indexFilePath = `${directory.getPath()}/index.ts`;
+  const indexFile = directory.getSourceFile(indexFilePath);
+  if (!indexFile) {
+    const sourceCode = `export * from './${directory.getBaseName()}'`
+    const file = directory.createSourceFile(indexFilePath, sourceCode, {overwrite:true})
+
+    file.save()
+  }
+});
 
 /* files.forEach((sourceFile) => {
   const importDeclarations = sourceFile.getImportDeclarations();
